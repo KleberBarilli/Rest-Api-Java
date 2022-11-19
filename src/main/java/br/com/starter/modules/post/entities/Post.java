7eb9085c6a3d4b4c259c5f9d3d28975662fd0b32
kleber.barilli <kleber.barilli73@gmail.com>
2022-11-19 17:43:05 -0300

    entity posts table

---
+++ src/main/java/br/com/starter/modules/post/entities/Post.java
@@ -0,0 +1,39 @@
+package br.com.starter.modules.post.entities;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+@Table(name = "posts", uniqueConstraints = { @UniqueConstraint(columnNames = { "title" }) })
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String content;
+
+    private Date createdAt;
+
+}
