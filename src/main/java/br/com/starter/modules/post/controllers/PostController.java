9e4e58225baac94c9530d389fb77a26a8ec2c121
kleber.barilli <kleber.barilli73@gmail.com>
2022-12-23 11:45:04 -0300

    feat: create post controller

---
+++ src/main/java/br/com/starter/modules/post/controllers/PostController.java
@@ -0,0 +1,27 @@
+package br.com.starter.modules.post.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.starter.modules.post.dtos.PostDto;
+import br.com.starter.modules.post.services.PostService;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+}
