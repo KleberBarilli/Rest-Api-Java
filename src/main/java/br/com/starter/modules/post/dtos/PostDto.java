0beeb53532b35c9ab2e223ca10d93ac169939c5e
kleber.barilli <kleber.barilli73@gmail.com>
2022-12-23 11:24:30 -0300

    feat: postDto

---
+++ src/main/java/br/com/starter/modules/post/dtos/PostDto.java
@@ -0,0 +1,10 @@
+package br.com.starter.modules.post.dtos;
+
+public class PostDto {
+
+    private long id;
+    private String title;
+    private String description;
+    private String content;
+
+}
