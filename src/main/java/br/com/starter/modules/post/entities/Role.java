80cb50ec1683f3eb741cdcef241171889d82dc46
kleber.barilli <kleber.barilli73@gmail.com>
2023-06-06 12:12:23 -0300

    wip

---
+++ src/main/java/br/com/starter/modules/post/entities/Role.java
@@ -0,0 +1,27 @@
+package br.com.starter.modules.post.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
