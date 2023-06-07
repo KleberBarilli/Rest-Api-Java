92944e0bb018e7ef60529b821da810b001b786bb
kleber.barilli <kleber.barilli73@gmail.com>
2023-06-07 10:23:26 -0300

    auth controller

---
+++ src/main/java/br/com/starter/modules/post/controllers/AuthController.java
@@ -0,0 +1,40 @@
+package br.com.starter.modules.post.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.starter.modules.post.dtos.JwtAuthResponse;
+import br.com.starter.modules.post.dtos.LoginDto;
+import br.com.starter.modules.post.dtos.RegisterDto;
+import br.com.starter.modules.post.services.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    private AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping(value = { "/login", "/signin" })
+    public ResponseEntity<JwtAuthResponse> login(@RequestBody LoginDto loginDto) {
+        String token = authService.login(loginDto);
+
+        JwtAuthResponse jwtAuthResponse = new JwtAuthResponse();
+        jwtAuthResponse.setAccessToken(token);
+
+        return ResponseEntity.ok(jwtAuthResponse);
+    }
+
+    @PostMapping(value = { "/register", "/signup" })
+    public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
+        String response = authService.register(registerDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+}
